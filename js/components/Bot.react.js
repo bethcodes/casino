5ff4c17f18405c4d0935ec66c4cc3ae354bd9fe0
beth <bandresbeck@gmail.com>
2015-11-20 00:12:00 -0500

    implementing bot algorithm

--- js/components/Bot.react.js
+++ js/components/Bot.react.js
@@ -9,47 +9,59 @@ var Bot = React.createClass({
     HistoryStore.addChangeListener(this.onPullMade);
   },
 
-  getBeliefs: function() {
+  getInitialBeliefs: function() {
     var bandits = HistoryStore.getBanditIndexes();
-    var beliefFunction = function() { return 4; };
-    var beliefs = bandits.map(function(bandit_id) {
-        return {
+    var beliefs = {}
+    bandits.forEach(function(bandit_id) {
+        beliefs[bandit_id] = {
             bandit_id: bandit_id,
-            beliefFunction: beliefFunction
-        }
+            beliefValue: 4,
+            observations: 1
+        };
     });
 
     return beliefs;
   },
 
-  getNextPull: function() {
-    var best_bandit;
-    var best_payoff = -100;
-    this.state.beliefs.forEach(function(belief) {
-        var payoff = belief.beliefFunction();
-        if (payoff > best_payoff) {
-            best_bandit = belief.bandit_id;
-            best_payoff = payoff;
-        } else if (payoff === best_payoff) {
-
-        }
-    });
-
-    return "3";
-  },
-
   getInitialState: function() {
     return {
       payoff: 0,
-      beliefs: this.getBeliefs()
+      beliefs: this.getInitialBeliefs()
     };
   },
 
+  getNextPull: function() {
+    var best_bandit;
+    var best_payoff = -100;
+    Object.keys(this.state.beliefs).forEach(function(key) {
+        var belief = this.state.beliefs[key];
+        var payoff = belief.beliefValue;
+        if (payoff > best_payoff ||
+          (payoff === best_payoff && Math.random() > 0.5)) {
+            best_bandit = belief.bandit_id;
+            best_payoff = payoff;
+        }
+    }.bind(this));
+
+    return best_bandit;
+  },
+
+  updatedBeliefs: function(bandit_id, payoff) {
+    var beliefs = this.state.beliefs;
+    var belief = beliefs[bandit_id];
+    belief.beliefValue = ((belief.beliefValue * belief.observations)+payoff) / (belief.observations+1);
+    belief.observations += 1;
+    beliefs[bandit_id] = belief;
+    return beliefs;
+  },
+
   onPullMade: function() {
-    var bandit = this.getNextPull();
-    var payoff = HistoryStore.getPayoffForBandit(bandit);
-    console.log("got payoff "+payoff+" for bandit "+bandit);
-    this.setState({payoff: this.state.payoff + payoff});
+    var bandit_id = this.getNextPull();
+    var payoff = HistoryStore.getPayoffForBandit(bandit_id);
+    var newBeliefs = this.updatedBeliefs(bandit_id, payoff);
+
+    console.log("got payoff "+payoff+" for bandit "+bandit_id);
+    this.setState({payoff: this.state.payoff + payoff, beliefs: newBeliefs});
   },
 
   render: function() {
